4c51e587ed717d399946cd5b09eceba742317224
mubashir-73 <mubashirsheriff7@gmail.com>
2025-12-12 09:45:35 +0530

    Create hash --> move to bcryt later

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,7 +1,9 @@
 import {z} from  'zod/v4';
 
 const createUserSchema = z.object({
-    email: z.string().min(1,"Email is required"),
-    name: z.string().min(1,"Name is required"),
+    email: z.string("email must be a string").min(1,"Email is required"),
+    name: z.string("name must be a string").min(1,"Name is required"),
     password: z.string().min(1,"Password is required"),
 })
+
+export type CreateUserInput = z.infer<typeof createUserSchema>;
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,7 @@
 import prisma from "../../utils/prisma.js";
+import type { CreateUserInput } from "./user.schema.ts";
 
-export async function createUser(input) {
+export async function createUser(input: CreateUserInput) {
   const user = await prisma.user.create({
     data: input,
   });
